881c62d29309058a058d6be2a3d5feb9559f1d63
Cozak92 <Cozak92@gmail.com>
2020-08-27 22:03:47 +0900

    v0.12

--- management/src/App.js
+++ management/src/App.js
@@ -1,6 +1,24 @@
 import React, { Component} from 'react';
 import './App.css';
-import Customer from './components/customer'
+import Customer from './components/customer';
+import Table from '@material-ui/core/Table';
+import Paper from '@material-ui/core/Paper'; //외부를 감싸기 위한 컴포넌트
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root : {
+    width : '100%',
+    marginTop : theme.spacing.unit * 3,
+    overFlowX : 'auto' //X축으로 오버플로우
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -33,24 +51,44 @@ const customers = [
 ]
 
 class App extends Component {
+  
   render() {
+    const { classes } = this.props;
     return (
-      customers.map(member => { // 맵을 이용해서 다수의 정보를 출력할때는 key를 사용해야함
-        return (
-          <Customer 
-            key = {member.id}
-            id = {member.id}
-            avatar = {member.avatar}
-            name = {member.name}
-            birthday = {member.birthday}
-            gender = {member.gender}
-            loaction = {member.loaction}
-            job = {member.job}
-        />
-        )
-      })
+      <Paper ClassName = {classes.root}>
+      
+        <Table ClassName = {classes.table}>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>지역</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+
+          <TableBody>
+            {customers.map(member => { // 맵을 이용해서 다수의 정보를 출력할때는 key를 사용해야함
+                return (
+                  <Customer 
+                    key = {member.id}
+                    id = {member.id}
+                    avatar = {member.avatar}
+                    name = {member.name}
+                    birthday = {member.birthday}
+                    gender = {member.gender}
+                    location = {member.loaction}
+                    job = {member.job}
+                  />
+                )
+            })}
+          </TableBody>
+        </Table>
+        
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- management/src/components/customer.js
+++ management/src/components/customer.js
@@ -1,51 +1,49 @@
 import React from 'react';
+import TableRow from "@material-ui/core/TableRow"
+import TableCell from "@material-ui/core/TableCell"
 
 
 class Customer extends React.Component {
     render() { //props 컴포넌트를 계층적으로 구성 // JSX를 사용해서 반환
         return (
-        <div>
-            <CustomerProfile
-                name = {this.props.name}
-                id = {this.props.id}
-                avatar = {this.props.avatar}
-            />
-            <CustomerInfo 
-                birthday = {this.props.birthday}
-                loaction ={this.props.loaction}
-                gender ={this.props.gender}
-                job = {this.props.job}
-            />
-        </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src = {this.props.avatar} alt="profile"></img></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.location}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
 
     }
 }
 
-class CustomerProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src = {this.props.avatar} alt="profile"></img>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
+// class CustomerProfile extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <img src = {this.props.avatar} alt="profile"></img>
+//                 <h2>{this.props.name}({this.props.id})</h2>
+//             </div>
 
-        )
-    }
-}
+//         )
+//     }
+// }
 
-class CustomerInfo extends React.Component{
-    render(){
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.loaction}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
+// class CustomerInfo extends React.Component{
+//     render(){
+//         return (
+//             <div>
+//                 <p>{this.props.birthday}</p>
+//                 <p>{this.props.loaction}</p>
+//                 <p>{this.props.gender}</p>
+//                 <p>{this.props.job}</p>
+//             </div>
+//         )
+//     }
+// }
 
 
 export default Customer;
